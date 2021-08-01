282deabc5a02d3569018653f9383a6ef09a6cdab
Mike Reimer <themichaelreimer@gmail.com>
2021-07-31 20:50:03 -0500

    Add files via upload

--- player.lua
+++ player.lua
@@ -2,13 +2,18 @@ isSwinging = false
 swordObject = {} 
 
 function drawPlayer(player)
-  local playerStep = 2*math.sin((player:getX() + player:getY())/4)
+  local px = player:getX()
+  local py = player:getY()
+  local playerStep = 2*math.sin((px + py)/4)
   local tSize = screen.tileSize
   local swordBody = swordObject.body
+  
   love.graphics.setColor(colours.green) -- nord green
-  love.graphics.print("@", player:getX()-tSize/2, player:getY()-tSize/2 + playerStep, player:getAngle())
+  love.graphics.print("@", px-tSize/2, py-tSize/2 + playerStep, player:getAngle())
   if isSwinging then
-    love.graphics.print("t", swordBody:getX() - swordObject.width/2, swordBody:getY() - swordObject.height, swordBody:getAngle()+math.pi)
+    local swordOriginX = swordBody:getX() - swordObject.width/2 
+    local swordOriginY = swordBody:getY() - swordObject.height/2 + tSize/2
+    love.graphics.print("t", swordOriginX, swordOriginY, swordBody:getAngle(), 1, 1, swordObject.width/2, swordObject.height/2)
   end
   
     -- DEBUG 
@@ -52,6 +57,10 @@ function playerUpdate(dt, body)
   end
   
   player:setLinearVelocity(dx,dy)
+  if isSwinging then
+    swordObject.timer = swordObject.timer - dt
+    if swordObject.timer < 0 then endSwingSword() end
+  end
 end
 
 function swingSword()
@@ -60,12 +69,22 @@ function swingSword()
       body = love.physics.newBody(world, player:getX(), player:getY(), "dynamic"),
       shape =  love.physics.newRectangleShape(8,36),
       width = 8,
-      height = 24,
+      height = 12,
+      timer = 0.25,
     }
   swordObject.fixture = love.physics.newFixture(swordObject.body, swordObject.shape, 10)
   swordObject.fixture:setMask(collisionCategories.player) -- Might not be necessary with the joint
-  swordObject.joint = love.physics.newMotorJoint(player, swordObject.body)
-  swordObject.joint:setAngularOffset(0)
-  
-  
+  swordObject.joint = love.physics.newRevoluteJoint(player, swordObject.body, player:getX(), player:getY(), false)
+  swordObject.joint:setMotorSpeed(20)
+  swordObject.joint:setMaxMotorTorque(1000000)
+  swordObject.joint:setMotorEnabled(true)
+end
+
+function endSwingSword()
+  swordObject.joint:destroy()
+  swordObject.fixture:destroy()
+  --swordObject.shape:destroy()
+  swordObject.body:destroy()
+  swordObject = {}
+  isSwinging = false
 end
