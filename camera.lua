3f5697cb23740ead286e496a79352c8a2812c1f9
themichaelreimer <themichaelreimer@gmail.com>
2021-08-07 22:56:32 -0500

    Basic UI

--- camera.lua
+++ camera.lua
@@ -1,32 +1,33 @@
 function makeCamera(world, xPixel, yPixel)
-  
-  -- Idea: Camera is just an invisible body, who's 
+
+  -- Idea: Camera is just an invisible body, who's
   -- position in the world determines the transformation matrix
   -- and who's position is constrained by the map
-  
+
   return love.physics.newBody(world, xPixel, yPixel, "dynamic")
-  
+
 end
 
 function moveCamera(camera, dt)
+  local uiSize = screen.height/5
   local halfWidth = screen.width/2
-  local halfHeight = screen.height/2
+  local halfHeight = (screen.height + uiSize)/2
   local halfTile = screen.tileSize/2
-  
+
   local tx = math.max(player.body:getX(), halfWidth - halfTile)
   tx = math.min(tx, map.widthPixels - halfWidth)
   local dx = tx - camera:getX() - halfWidth
-  
+
   local ty = math.max(player.body:getY(), halfHeight - halfTile)
   ty = math.min(ty, map.heightPixels - halfHeight )
   local dy = ty - camera:getY() - halfHeight
-  
+
   local dist2 = dx*dx + dy*dy
-  
+
   if dist2 > 0.1 then
     camera:setLinearVelocity(dx * dist2 * dt / 16, dy * dist2 * dt / 16)
   else
     camera:setLinearVelocity(0, 0)
   end
-  
+
 end
--- main.lua
+++ main.lua
@@ -24,16 +24,16 @@ collisionCategories = {
 function love.load()
   -- Init physics and world
   world = love.physics.newWorld(0, 0, true)
-  
+
   -- TODO: Determine start point in the world instead of this hardcoded spot
   local tilesWidth = 60
   local tilesHeight = 60
   local initX = 400
   local initY = 300
-  
+
   player = Player:new(nil, world, initX, initY)
   camera = makeCamera(world, initX, initY)
-  
+
   -- Screen Settings
   screen.tileSize = 24
   screen.width = screen.tileSize * 40 -- viewport width
@@ -41,11 +41,11 @@ function love.load()
   screen.settings = {
     resizable=true,
   }
-  
+
   font = love.graphics.newFont("VeraMono.ttf", screen.tileSize)
   love.graphics.setFont(font)
   love.window.updateMode(screen.width, screen.height, screen.settings)
-  
+
   -- Generate Level
   map = makeSimplexCave(tilesWidth, tilesHeight, caveGenParams)
   makePhysicsBody(map.map, world)
@@ -55,7 +55,7 @@ function love.load()
 end
 
 function love.update(dt)
-  
+
   moveCamera(camera, dt)
   player:update(dt)
   world:update(dt)
@@ -63,7 +63,7 @@ function love.update(dt)
 end
 
 function updateMapCanvas()
-  love.graphics.setColor(colours.white) -- nord white
+  love.graphics.setColor(colours.lightGray) -- nord white
   love.graphics.setCanvas(levelCanvas)
   local tileSize = screen.tileSize
   for y=0, #map.map do
@@ -71,10 +71,10 @@ function updateMapCanvas()
       love.graphics.print(map.map[y][x].char, x*tileSize, y*tileSize)
     end
   end
-  
+
   -- DEBUG REGION
   -- Draw bounding boxes for physics; can be deleted once physics works
-  if debugRender then 
+  if debugRender then
     love.graphics.setColor(0.5, 0.1, 0.1,0.5)
     bodies = world:getBodies()
     body = bodies[1]
@@ -83,17 +83,47 @@ function updateMapCanvas()
     end
     love.graphics.setColor(1, 1, 1, 1)
   end
-  
+
   love.graphics.setCanvas()
 end
 
 function love.draw(dt)
+
+  local uiSize = screen.height/5
+  local halfWidth = screen.width/2
+  local margin = 12
+  local lineHeight = 24
+
+  -- DRAW LEVEL
   love.graphics.setBackgroundColor(colours.black) -- nord black
-  love.graphics.translate(-camera:getX(), -camera:getY())
+  love.graphics.translate(-camera:getX(), -camera:getY()) -- Transform into camera space
   love.graphics.draw(levelCanvas)
   player:draw()
   love.graphics.setColor(colours.white) -- nord white
-  
+
+  love.graphics.translate(camera:getX(), camera:getY()) -- Transform back to screen space
+
+  -- DRAW GUI
+  love.graphics.translate(0, 4*uiSize) -- Transform into UI screen space
+  love.graphics.setColor(colours.black)
+  love.graphics.rectangle('fill', 0, 0, screen.width, uiSize)
+  love.graphics.setColor(colours.white)
+
+  love.graphics.print("Jerry the Destroyer", margin, lineHeight)
+  love.graphics.print("Dungeon: L1", margin + halfWidth, lineHeight)
+
+  love.graphics.print("Health: 24/24", margin, 2*lineHeight)
+  love.graphics.setColor(colours.green)
+  love.graphics.print("[==========]", 216, 2*lineHeight)
+  love.graphics.setColor(colours.white)
+  love.graphics.print("Magic: 10/10", margin + halfWidth, 2*lineHeight)
+  love.graphics.setColor(colours.blue)
+  love.graphics.print("[==========]", 216 + halfWidth, 2*lineHeight)
+
+  love.graphics.setColor(colours.white)
+  love.graphics.print("X: Sword", margin, 3*lineHeight)
+  love.graphics.print("Z:", margin, 4*lineHeight)
+
 end
 
 function love.resize(width, height)
