01dfc2e956aa61d7d1666a50c4a530675a7b9161
themichaelreimer <themichaelreimer@gmail.com>
2021-09-12 09:37:03 -0500

    add comments

--- src/ecs.lua
+++ src/ecs.lua
@@ -4,5 +4,9 @@ updateSystem = require("src.systems.updateSystem")
 drawSystem = require("src.systems.drawSystem")
 lifetimeSystem = require("src.systems.lifetimeSystem")
 
+-- ECS world, as opposed to the physics world
 ecsWorld = tiny.world(lifetimeSystem, updateSystem, drawSystem)
+
+-- Table of all ECS entities currently loaded
+-- This table is cleaned by updateSystem every frame
 gameObjects = {}
