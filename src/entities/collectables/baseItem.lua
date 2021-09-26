23b5520caae80ffbe9702b5b553c8af0deb21176
themichaelreimer <themichaelreimer@gmail.com>
2021-09-26 15:18:03 -0500

    Base Item and inheritence

---
+++ src/entities/collectables/baseItem.lua
@@ -0,0 +1,44 @@
+local Item = class("Item")
+
+-- OVERRIDE THESE PARAMS, AND "Item:payload()"
+Item.char = 'i'
+Item.itemName = "item" -- Display name
+Item.acquireString = "[Base Item does nothing]"
+
+
+Item.minTextTime = 2.0
+
+function Item:init(x, y)
+  self.colour = colours.yellow
+  self.deleted = false
+
+  self.body = love.physics.newBody(world, x, y, "dynamic")
+  self.shape = love.physics.newRectangleShape(-5, 5, 22, 22)
+  self.fixture = love.physics.newFixture(self.body, self.shape, 1)
+  self.fixture:setSensor(true)
+  self.fixture:setUserData(self)
+end
+
+-- OVERRIDE ME
+function Item:payload()
+end
+
+function Item:collect()
+  self:payload()
+  local acquireText = self.itemName .. " acquired!"
+  setBlockingText(acquireText, self.acquireString, self.minTextTime)
+  self:destroy()
+end
+
+function Item:destroy()
+  self.deleted = true
+  self.fixture:destroy()
+  self.shape:release()
+  self.body:destroy()
+end
+
+function Item:dealHit(other)
+  if other.isPlayer then self:collect() end
+end
+
+return Item
