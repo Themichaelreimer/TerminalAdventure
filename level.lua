e5cea3486cd51d3ef85d3db7a95d422bba9061d5
themichaelreimer <themichaelreimer@gmail.com>
2021-09-30 09:36:49 -0500

    Make plush less dangerous

--- level.lua
+++ level.lua
@@ -34,15 +34,15 @@ function Level:new(o, world, floorNum)
   self:makePhysicsBody()
   self:makeLevelBoundaryCollider()
 
-  for i=0, 5 do
+  for i=0, 1 do
     self:placeEnemyInLevel("Snake")
   end
 
-  for i=0, 5 do
+  for i=0, 1 do
     self:placeEnemyInLevel("Jackal")
   end
 
-  for i=0, 5 do
+  for i=0, 15 do
     self:placeEnemyInLevel("Plush")
   end
 
--- src/entities/enemies/plush.lua
+++ src/entities/enemies/plush.lua
@@ -6,6 +6,7 @@ Plush.char = 'a'
 Plush.colourName = 'pink'
 Plush.speed = 8
 Plush.dashSpeed = 10
+Plush.damage=3
 
 Plush.IDLE_TIME = 0.01
 Plush.MOVE_TIME = 1.0
